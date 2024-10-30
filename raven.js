dd416489b63b44d38377d475c4843895d5c12751
Nick Hunter <137078621+HunterNick2@users.noreply.github.com>
2024-10-30 09:52:37 +0300

    Update raven.js

--- raven.js
+++ raven.js
@@ -538,6 +538,25 @@ reply('An error occured while communicating with the APIs\n' + e);
 }
 }
 break;
+	      case "screenshot": case "ss": {
+		      try {
+let cap = `Screenshot by ${botname}`
+
+if (!text) return m.reply("Provide a website link to screenshot.")
+
+const image = `https://image.thum.io/get/fullpage/${text}`
+
+await client.sendMessage(m.chat, { image: { url: image }, caption: cap}, {quoted: m });
+
+
+} catch (error) {
+
+m.reply("An error occured.")
+
+}
+
+	      }
+	      break;
 
    case "gpt2": {
 const { G4F } = require("g4f"); 
