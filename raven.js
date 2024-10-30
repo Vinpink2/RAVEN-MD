c37a900d805d0a9391646d1effa342ca7fed8341
Nick Hunter <137078621+HunterNick2@users.noreply.github.com>
2024-10-30 19:17:18 +0300

    Update raven.js

--- raven.js
+++ raven.js
@@ -1209,7 +1209,7 @@ try {
 const response = await fetch(`https://widipe.com//download/tikdl?url=${text}`);
 const data = await response.json();
 
-const tikvid = data;
+const tikvid = data.data.no_wm;
 await client.sendMessage(m.chat,{video : {url : tikvid },caption : `Downloaded by ${botname}`,gifPlayback : false },{quoted : m}) 
 
 } catch (e) {
