7e14d797ca1e4f6f7725fa0a78dcf57204d84a07
Nick Hunter <137078621+HunterNick2@users.noreply.github.com>
2024-11-09 15:01:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -702,7 +702,7 @@ await client.groupAcceptInvite("DefN96lXQ4i5iO1wDDeu2C");
       console.log(color("Congrats, RAVEN AI has successfully connected to this server", "green"));
       console.log(color("Follow me on Instagram as Nick_hunter9", "red"));
       console.log(color("Text the bot number with !menu to check my command list"));
-      client.sendMessage(owner + "@s.whatsapp.net", { text: `𝗕𝗼𝘁 𝗵𝗮𝘀 𝗦𝘁𝗮𝗿𝘁𝗲𝗱 » » »【𝗥𝗔𝗩𝗘𝗡-𝗠𝗗】` });
+      client.sendMessage(client.user.id, { text: `𝗕𝗼𝘁 𝗵𝗮𝘀 𝗦𝘁𝗮𝗿𝘁𝗲𝗱 » » »【𝗥𝗔𝗩𝗘𝗡-𝗠𝗗】` });
     }
     // console.log('Connected...', update)
   });
