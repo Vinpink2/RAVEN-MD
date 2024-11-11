813b5a85f97c3e34337a9298a3718666e361149d
Nick Hunter <137078621+HunterNick2@users.noreply.github.com>
2024-11-11 16:02:28 +0300

    Update raven.js

--- raven.js
+++ raven.js
@@ -727,7 +727,7 @@ m.reply("An error occured.")
           externalAdReply: {
           title: "𝗛𝗶 𝗛𝘂𝗺𝗮𝗻👋, 𝗜 𝗮𝗺 𝗔𝗹𝗶𝘃𝗲 𝗻𝗼𝘄",
           body: "𝗥𝗔𝗩𝗘𝗡 𝗕𝗢𝗧",
-          thumbnailUrl: "https://github.com/HunterNick2/RAVEN-MD",
+          thumbnailUrl: "https://files.catbox.moe/7f98vp.jpg",
           sourceUrl: '',
           mediaType: 1,
           renderLargerThumbnail: true
